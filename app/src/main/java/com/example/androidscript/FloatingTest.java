8ad3ad6108ee7ab4180d05d4b2ba12fa9c334315
AndyDu01 <andy255060@gmail.com>
2021-07-12 16:18:12 +0800

    fixArchitech

--- app/src/main/java/com/example/androidscript/FloatingTest.java
+++ app/src/main/java/com/example/androidscript/FloatingTest.java
@@ -32,14 +32,6 @@ public class FloatingTest extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_floating_test);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
--- app/src/main/java/com/example/androidscript/MainActivity.java
+++ app/src/main/java/com/example/androidscript/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "In MainActivity", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
