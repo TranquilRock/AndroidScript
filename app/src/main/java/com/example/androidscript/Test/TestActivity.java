b1f2dbc3b667233d8c0ec8ac575bf8101d3020e1
WEN-Hao-Wu <dfgh60405@gmail.com>
2021-07-13 18:10:03 +0800

    Screenshot prototype

--- app/src/main/java/com/example/androidscript/Test/TestActivity.java
+++ app/src/main/java/com/example/androidscript/Test/TestActivity.java
@@ -1,19 +1,26 @@
 package com.example.androidscript.Test;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.androidscript.util.AutoClick;
 import com.example.androidscript.R;
+import com.example.androidscript.util.ScreenShot;
 
-public class ClickerActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clicker);
+        setContentView(R.layout.activity_test);
+        /*Button scnShot = findViewById(R.id.scnshot);
+        scnShot.setOnClickListener(v -> ScreenShot.getScreenShot());*/
 
     }
 
@@ -23,5 +30,10 @@ public class ClickerActivity extends AppCompatActivity {
 
     public void Autoclick_swipe(View view){
         AutoClick.autoClickPos(540, 1310, 545, 1315);
+    }
+    public void scnShot(View view){
+        Bitmap scn = ScreenShot.getScreenShot(view);
+        ImageView igShow = findViewById(R.id.imageView);
+        igShow.setImageBitmap(scn);
     }
 }
