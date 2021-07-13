0652c053ab79b8f1b950bb987a82f767dd823075
AndyDu01 <andy255060@gmail.com>
2021-07-13 16:35:52 +0800

    Add select file

--- app/src/main/java/com/example/androidscript/Menu/MenuActivity.java
+++ app/src/main/java/com/example/androidscript/Menu/MenuActivity.java
@@ -1,16 +1,42 @@
 package com.example.androidscript.Menu;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.androidscript.R;
+import com.example.androidscript.util.BtnMaker;
+
+import static android.content.ContentValues.TAG;
 
 public class MenuActivity extends AppCompatActivity {
+    private Button btnToCreate;
+    private Button btnToLoad;
+    private EditText etNewName;
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        etNewName = (EditText) findViewById(R.id.et_New_Name);
+        btnToLoad = BtnMaker.performIntent(R.id.btn_To_Load,this, Intent.ACTION_GET_CONTENT);
+        output = (TextView) findViewById(R.id.output);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && data.getData()!= null){
+            output.setText(data.getDataString());
+        }
+        else{
+            Log.d(TAG, "onTEQ: ");
+        }
     }
 }
--- app/src/main/java/com/example/androidscript/util/BtnMaker.java
+++ app/src/main/java/com/example/androidscript/util/BtnMaker.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BtnMaker {
@@ -16,6 +17,17 @@ public class BtnMaker {
             public void onClick(View v) {
                 Intent intent = new Intent(from, to);
                 from.startActivity(intent);
+            }
+        }));
+        return btn;
+    }
+    public static Button performIntent(int id,AppCompatActivity from ,String command){
+        Button btn = (Button)(from.findViewById(id));
+        btn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(command);
+                from.startActivityForResult(intent,0);
             }
         }));
         return btn;
