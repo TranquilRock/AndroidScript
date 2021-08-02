defc389d72d14eeb0138ee85009a2e813a0fd137
AndyDu01 <andy255060@gmail.com>
2021-08-02 09:15:28 +0800

    Fix division bug and function name

--- app/src/main/java/com/example/androidscript/Menu/FGO/FGOEditor.java
+++ app/src/main/java/com/example/androidscript/Menu/FGO/FGOEditor.java
@@ -2,9 +2,7 @@ package com.example.androidscript.Menu.FGO;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,7 +13,6 @@ import com.example.androidscript.UserInterface.UIActivity;
 import com.example.androidscript.util.BtnMaker;
 import com.example.androidscript.util.FileOperation;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Vector;
@@ -29,20 +26,21 @@ public class FGOEditor extends UIActivity {
     public static final String[] EndBlock = {"End"};
 
 
-
-    public static String linx(int x, int w, int m){
-        return Integer.toString((int)w/2+m*(x-960));
+    public static String transform_x(int x, int w, float m) {
+        return Integer.toString((int) (w / 2.0 + m * (x - 960)));
     }
-    public static String liny(int y, int h, int m){
-        return Integer.toString((int)h/2+m*(y-600));
+
+    public static String transform_y(int y, int h, float m) {
+        return Integer.toString((int) (h / 2.0 + m * (y - 600)));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        BtnMaker.registerOnClick(R.id.start_service, this, v -> startActivity(new Intent(this, StartService.class).putExtra("Orientation","Landscape")));
+        BtnMaker.registerOnClick(R.id.start_service, this, v -> startActivity(new Intent(this, StartService.class).putExtra("Orientation", "Landscape")));
         BtnMaker.registerOnClick(R.id.start_floating, this, v -> StartService.startFloatingWidget(this));
     }
+
     @Override
     protected void setRecycleButton() {
         this.ButtonView.setLayoutManager(new GridLayoutManager(this, 4));
@@ -92,59 +90,55 @@ public class FGOEditor extends UIActivity {
         return "";
     }
 
-    public static void savetointp(String filename, Vector<Vector<String>> data, int w, int h) {
-        if(h > w){
+    public static void compile(String filename, Vector<Vector<String>> data, int w, int h) {
+        if (h > w) {
             int tmp = w;
             w = h;
             h = tmp;
         }
-        int m = 1;
-        if(w/h < 16/9){
-            m = w/1920;
-        }else{
-            m = h/1200;
+        float m = 1;
+        if (w * 9 < h * 16) {
+            m = (float) w / 1920;
+        } else {
+            m = (float) h / 1200;
         }
 
-        Vector<String> save = new Vector<String>();
+        Vector<String> save = new Vector<>();
 
-        for(int i=0; i<data.size(); i++){
-            switch (data.get(i).get(0)){
-                case"PreStage":
-                    save.add("Click "+linx(1400, w, m)+" "+liny(320, h, m));
-                    switch (data.get(i).get(1)){
-                        case"0":
-                            save.add("Click "+linx(960, w, m)+" "+liny(330, h, m));
-                            break;
-                        case"1":
-                            save.add("Click "+linx(960, w, m)+" "+liny(540, h, m));
-                            break;
-                        case"2":
-                            save.add("Click "+linx(960, w, m)+" "+liny(750, h, m));
-                            break;
-                        case"3":
-                            save.add("Swipe "+linx(960, w, m)+" "+liny(786, h, m)+" "+linx(960, w, m)+" "+liny(516, h, m));
-                            save.add("Click "+linx(960, w, m)+" "+liny(320, h, m));
-                            break;
-                    }
-                    save.add("Click "+linx(960, w, m)+" "+liny(320, h, m));
-                    switch (data.get(i).get(2)){
-                        case"0":
-                            save.add("Click "+linx(800, w, m)+" "+liny(467, h, m));
-                            break;
-                        case"1":
-                            save.add("Click "+linx(960, w, m)+" "+liny(540, h, m));
-                            break;
-                        case"2":
-                            save.add("Click "+linx(960, w, m)+" "+liny(750, h, m));
-                            break;
-                        case"3":
-                            save.add("Swipe "+linx(960, w, m)+" "+liny(786, h, m)+" "+linx(960, w, m)+" "+liny(516, h, m));
-                            save.add("Click "+linx(960, w, m)+" "+liny(320, h, m));
-                            break;
-                    }
-                    break;
-
-
+        for (Vector<String> block : data) {
+            if ("PreStage".equals(block.get(0))) {
+                save.add("Click " + transform_x(1400, w, m) + " " + transform_y(320, h, m));
+                switch (block.get(1)) {
+                    case "0":
+                        save.add("Click " + transform_x(960, w, m) + " " + transform_y(330, h, m));
+                        break;
+                    case "1":
+                        save.add("Click " + transform_x(960, w, m) + " " + transform_y(540, h, m));
+                        break;
+                    case "2":
+                        save.add("Click " + transform_x(960, w, m) + " " + transform_y(750, h, m));
+                        break;
+                    case "3":
+                        save.add("Swipe " + transform_x(960, w, m) + " " + transform_y(786, h, m) + " " + transform_x(960, w, m) + " " + transform_y(516, h, m));
+                        save.add("Click " + transform_x(960, w, m) + " " + transform_y(320, h, m));
+                        break;
+                }
+                save.add("Click " + transform_x(960, w, m) + " " + transform_y(320, h, m));
+                switch (block.get(2)) {
+                    case "0":
+                        save.add("Click " + transform_x(800, w, m) + " " + transform_y(467, h, m));
+                        break;
+                    case "1":
+                        save.add("Click " + transform_x(960, w, m) + " " + transform_y(540, h, m));
+                        break;
+                    case "2":
+                        save.add("Click " + transform_x(960, w, m) + " " + transform_y(750, h, m));
+                        break;
+                    case "3":
+                        save.add("Swipe " + transform_x(960, w, m) + " " + transform_y(786, h, m) + " " + transform_x(960, w, m) + " " + transform_y(516, h, m));
+                        save.add("Click " + transform_x(960, w, m) + " " + transform_y(320, h, m));
+                        break;
+                }
             }
         }
         FileOperation.writeToFile(filename, save);
