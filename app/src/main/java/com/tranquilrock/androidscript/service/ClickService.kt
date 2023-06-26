bf16986b9f7132681b8574300cb8ca0ae1a0b24b
TranquilRock <andy255060@gmail.com>
2023-06-26 12:19:07 +0800

    Change suspend to coroutine

--- app/src/main/java/com/tranquilrock/androidscript/service/ClickService.kt
+++ app/src/main/java/com/tranquilrock/androidscript/service/ClickService.kt
@@ -8,70 +8,83 @@ import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class ClickService : AccessibilityService() {
 
     public override fun onServiceConnected() {
         super.onServiceConnected()
         instance = this
-        Log.d(LOG_TAG, "onServiceConnected")
+        Log.d(TAG, "onServiceConnected")
     }
 
+    class OffException : Exception("AccessibilityService Not On!")
+
+    companion object {
+        private val TAG = ClickService::class.java.simpleName
+        private var instance: ClickService? = null
+
+        val running: Boolean
+            get() = instance != null
+
+        suspend fun click(x: Int, y: Int) {
+            val path = Path().apply {
+                moveTo((x - 1).toFloat(), (y - 1).toFloat())
+                lineTo((x + 1).toFloat(), (y + 1).toFloat())
+            }
+            susDispatch(
+                GestureDescription.Builder()
+                    .addStroke(StrokeDescription(path, 0, 1000))
+                    .build()
+            )
+        }
+
+        suspend fun swipe(x1: Int, y1: Int, x2: Int, y2: Int) {
+            val path = Path().apply {
+                moveTo(x1.toFloat(), y1.toFloat())
+                lineTo(x2.toFloat(), y2.toFloat())
+            }
+            susDispatch(
+                GestureDescription.Builder()
+                    .addStroke(StrokeDescription(path, 0, 1000))
+                    .build()
+            )
+        }
+
+        private suspend fun susDispatch(gestureDescription: GestureDescription) {
+            instance?.run {
+                suspendCoroutine<Unit?> { continuation ->
+                    dispatchGesture(
+                        gestureDescription,
+                        object : GestureResultCallback() {
+                            override fun onCompleted(gestureDescription: GestureDescription?) =
+                                continuation.resume(null)
+
+                            override fun onCancelled(gestureDescription: GestureDescription?) {
+                                Log.i(TAG, "Cancelled: $gestureDescription")
+                                continuation.resume(null)
+                            }
+                        },
+                        null,
+                    )
+                }
+            } ?: throw OffException()
+        }
+    }
+
+
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
-        Log.d(LOG_TAG, "onAccessibilityEvent")
+        Log.d(TAG, "onAccessibilityEvent")
     }
 
     override fun onInterrupt() {
         instance = null
-        Log.d(LOG_TAG, "onInterrupt")
+        Log.d(TAG, "onInterrupt")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(LOG_TAG, "onDestroy")
+        Log.d(TAG, "onDestroy")
     }
-
-    companion object {
-        private val LOG_TAG = ClickService::class.java.simpleName
-        private var instance: ClickService? = null
-
-        fun running(): Boolean {
-            return (instance != null)
-        }
-
-        suspend fun click(x: Int, y: Int) {
-            val path = Path()
-            path.moveTo((x - 1).toFloat(), (y - 1).toFloat())
-            path.lineTo((x + 1).toFloat(), (y + 1).toFloat())
-            instance?.run {
-                withContext(Dispatchers.Default) {
-                    dispatchGesture(
-                        GestureDescription.Builder().addStroke(StrokeDescription(path, 0, 100))
-                            .build(),
-                        null,
-                        null
-                    )
-                }
-            } ?: throw OffException()
-        }
-
-        suspend fun swipe(x1: Int, y1: Int, x2: Int, y2: Int) {
-            val path = Path()
-            path.moveTo(x1.toFloat(), y1.toFloat())
-            path.lineTo(x2.toFloat(), y2.toFloat())
-            instance?.run {
-                withContext(Dispatchers.Default) {
-                    dispatchGesture(
-                        GestureDescription.Builder().addStroke(StrokeDescription(path, 0, 1000))
-                            .build(),
-                        null,
-                        null
-                    )
-                }
-            } ?: throw OffException()
-
-        }
-    }
-
-    class OffException : Exception()
 }
